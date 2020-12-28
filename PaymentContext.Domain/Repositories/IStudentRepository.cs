edfb4cfeeab985634c8cca3c4402e3e84f8562d9
Lucas Moraes <lucasdemoraesc@gmail.com>
2020-12-28 15:35:31 -0300

    Aula 19: Repository Pattern

---
+++ PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,9 @@
+namespace PaymentContext.Domain.Repositories
+{
+	public interface IStudentRepository
+	{
+		bool DocumentExists(string document);
+		bool EmailExists(string email);
+		void CreateSubscription(Student student);
+	}
+}
